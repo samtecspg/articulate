14979388c722405b35fc69da85a6a50ef3203d81
Malavé <luis.malave@gmail.com>
2018-03-07 17:37:10 +0900

    Edit page had render issues due to incorrect data type in textPrompt
    Ref: #117

--- api/helpers/cast.js
+++ api/helpers/cast.js
@@ -1,8 +1,9 @@
 'use strict';
+const _ = require('lodash');
 
 module.exports = (object, type) => {
 
-    switch (type){
+    switch (type) {
 
         case 'agent':
             object.useWebhook = object.useWebhook === 'true';
@@ -20,14 +21,14 @@ module.exports = (object, type) => {
             object.useWebhook = object.useWebhook === 'true';
             object.examples = object.examples.map((example) => {
 
-                if (example.entities){
-                    if (example.entities.length === 0){
+                if (example.entities) {
+                    if (example.entities.length === 0) {
                         example.entities = [];
                     }
                     else {
                         example.entities = example.entities.map((entity) => {
 
-                            if (entity.entityId){
+                            if (entity.entityId) {
                                 entity.entityId = parseInt(entity.entityId);
                             }
                             entity.start = parseInt(entity.start);
@@ -40,34 +41,32 @@ module.exports = (object, type) => {
             });
             break;
         case 'scenario':
-            if (object.slots){
-                if (object.slots.length === 0){
+            console.log(`cast:scenario::textPrompts`); // TODO: REMOVE!!!!
+            console.log(object); // TODO: REMOVE!!!!
+            if (object.slots) {
+                if (object.slots.length === 0) {
                     object.slots = [];
                 }
                 else {
                     object.slots = object.slots.map((slot) => {
-
-                        if (slot.textPrompts){
-                            if (slot.textPrompts.length === 0){
-                                slot.textPrompts = [];
-                            }
-                        }
+                        if (!_.isArray(slot.textPrompts)) slot.textPrompts = [];
                         slot.isList = slot.isList === 'true';
                         slot.isRequired = slot.isRequired === 'true';
                         return slot;
                     });
                 }
             }
-            if (object.intentResponses){
-                if (object.intentResponses.length === 0){
+            if (object.intentResponses) {
+                if (object.intentResponses.length === 0) {
                     object.intentResponses = [];
                 }
             }
             break;
         case 'webhook':
             break;
-    };
-    if (object.id){
+    }
+    ;
+    if (object.id) {
         object.id = parseInt(object.id);
     }
     return object;
