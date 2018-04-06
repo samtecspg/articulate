c82c0563b6c9f384f11035a6f68a085ed0f4c047
Daniel Calvo <dcalvom@gmail.com>
2018-04-06 15:15:19 -0600

    New handlebars helpers included (Format.js and Comparison)
    (cherry picked from commit 42d51df55a3e84d7cc2aac576e9cc1340c3a8b72)

--- api/helpers/registerHandlebarsHelpers.js
+++ api/helpers/registerHandlebarsHelpers.js
@@ -1,5 +1,8 @@
 'use strict';
 const Moment = require('moment');
+const HandlebarsIntl = require('handlebars-intl');
+const helpers = require('handlebars-helpers');
+
 module.exports = (Handlebars) => {
 
     Handlebars.registerHelper('toJSON', (obj) => {
@@ -8,5 +11,10 @@ module.exports = (Handlebars) => {
     });
 
     Handlebars.registerHelper('dateTimeFormat', (datetime, format) => Moment(datetime).format(format));
+    HandlebarsIntl.registerWith(Handlebars);
+
+    const comparison = helpers.comparison({
+        handlebars: Handlebars
+    });
     return Handlebars;
 };
