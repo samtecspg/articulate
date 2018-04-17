5020a07659159f60d17f55eb92c739ea7b8a90ea
Daniel Calvo <dcalvom@gmail.com>
2018-04-16 19:45:04 -0600

    Added new hanldebars helpers and andList and orList helpers

--- api/helpers/registerHandlebarsHelpers.js
+++ api/helpers/registerHandlebarsHelpers.js
@@ -3,6 +3,17 @@ const Moment = require('moment');
 const HandlebarsIntl = require('handlebars-intl');
 const helpers = require('handlebars-helpers');
 
+const buildListOfWords = (words, separator) => {
+
+    if (words.length === 1){
+        return words[0];
+    }
+    if (words.length === 2){
+        return words[0] + ' ' + separator + ' ' + words[1];
+    }
+    return words.slice(0,words.length - 1).join(', ') + ', ' + separator + ' ' + words[words.length -1];
+};
+
 module.exports = (Handlebars) => {
 
     Handlebars.registerHelper('toJSON', (obj) => {
@@ -10,10 +21,20 @@ module.exports = (Handlebars) => {
         return JSON.stringify(obj, null, 2);
     });
 
+    Handlebars.registerHelper('andList', (words) => {
+
+        return buildListOfWords(words, 'and');
+    });
+
+    Handlebars.registerHelper('orList', (words) => {
+
+        return buildListOfWords(words, 'or');
+    });
+
     Handlebars.registerHelper('dateTimeFormat', (datetime, format) => Moment(datetime).format(format));
     HandlebarsIntl.registerWith(Handlebars);
 
-    helpers(['comparison', 'math'], {
+    helpers(['array', 'collection', 'comparison', 'inflection', 'math', 'misc', 'number', 'object', 'string', 'url'], {
         handlebars: Handlebars
     });
     return Handlebars;
