b80cdb8bc2dae66d822f38b24bcee63727832250
Daniel Calvo <dcalvom@gmail.com>
2018-06-15 15:25:39 -0600

    Refactoring of from value on duckling interval

--- api/modules/agent/tools/ducklingOutputToInterval.agent.tool.js
+++ api/modules/agent/tools/ducklingOutputToInterval.agent.tool.js
@@ -22,82 +22,50 @@ const ducklingOutputToInterval = (ducklingOutput, timezone) => {
             const time = isAnIntervalWithoutFinish ? Moment(output.value.from.value).tz(timezone) : Moment(output.value.value).tz(timezone);
             const todayTime = Moment().tz(timezone);
             const grain =  isAnIntervalWithoutFinish ? output.value.from.grain : output.value.grain;
+            newOutput.value.from = {
+                value: time.format(),
+                grain
+            };
             switch (grain) {
                 case 'second':
-                    newOutput.value.from = {
-                        value: time.format(),
-                        grain
-                    };
                     newOutput.value.to = {
                         value: isAnIntervalWithoutFinish ? time.add((todayTime.second() - time.second()), grain + 's').format() : time.add(1, grain + 's').format(),
                         grain
                     };
                     break;
                 case 'minute':
-                    newOutput.value.from = {
-                        value: time.format(),
-                        grain
-                    };
                     newOutput.value.to = {
                         value: isAnIntervalWithoutFinish ? time.add((todayTime.minute() - time.minute()), grain + 's').format() : time.add(1, grain + 's').format(),
                         grain
                     };
                     break;
                 case 'hour':
-                    newOutput.value.from = {
-                        value: time.format(),
-                        grain
-                    };
                     newOutput.value.to = {
                         value: isAnIntervalWithoutFinish ? time.add((todayTime.hour() - time.hour()), grain + 's').format() : time.add(1, grain + 's').format(),
                         grain
                     };
                     break;
                 case 'day':
-                    newOutput.value.from = {
-                        value: time.format(),
-                        grain
-                    };
                     newOutput.value.to = {
                         value: isAnIntervalWithoutFinish ? time.add((todayTime.day() - time.day()), grain + 's').format() : time.add(1, grain + 's').format(),
                         grain
                     };
                     break;
                 case 'week':
-                    newOutput.value.from = {
-                        value: time.format(),
-                        grain
-                    };
                     newOutput.value.to = {
                         value: isAnIntervalWithoutFinish ? time.add((todayTime.week() - time.week()), grain + 's').format() : time.add(1, grain + 's').format(),
                         grain
                     };
                     break;
                 case 'month':
-                    newOutput.value.from = {
-                        value: time.format(),
-                        grain
-                    };
-                    newOutput.value.to = {
-                        value: isAnIntervalWithoutFinish ? time.add((todayTime.month() - time.month()), grain + 's').format() : time.add(1, grain + 's').format(),
-                        grain
-                    };
                     break;
                 case 'year':
-                    newOutput.value.from = {
-                        value: time.format(),
-                        grain
-                    };
                     newOutput.value.to = {
                         value: isAnIntervalWithoutFinish ? time.add((todayTime.year() - time.year()), grain + 's').format() : time.add(1, grain + 's').format(),
                         grain
                     };
                     break;
                 case 'quarter':
-                    newOutput.value.from = {
-                        value: time.format(),
-                        grain
-                    };
                     newOutput.value.to = {
                         value: isAnIntervalWithoutFinish ? time.add(todayTime.months() - time.months(), grain + 's').format() : time.add(3, 'months').format(),
                         grain
