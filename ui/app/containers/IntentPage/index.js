1fab7721c72aaf2741235e73f0d036275b276172
Caleb M. Keller <calebmkeller@gmail.com>
2018-08-20 22:04:32 -0400

    Disable domain field on edit
    We can't support editing domains right at the moment so we will disable the field until we can.

--- ui/app/containers/IntentPage/index.js
+++ ui/app/containers/IntentPage/index.js
@@ -511,6 +511,7 @@ export class IntentPage extends React.PureComponent { // eslint-disable-line rea
                 value={intent.intentName}
                 onChange={(evt) => this.onChangeInput(evt, 'intentName')}
                 required
+                disabled = {this.state.editMode}
               />
               <FormTextInput
                 label={messages.userSaysTitle}
