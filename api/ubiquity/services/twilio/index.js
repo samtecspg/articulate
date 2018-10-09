d54b70224ff7c4eb49663ab34896a70ac920400f
Caleb M. Keller <calebmkeller@gmail.com>
2018-10-09 11:31:37 -0400

    Add sessionId to Ubiquity payload for Twilio
    @spg-matt FYI that I added this manually and probably messed up your merge in some way. :D

--- api/ubiquity/services/twilio/index.js
+++ api/ubiquity/services/twilio/index.js
@@ -76,6 +76,7 @@ module.exports = {
               text: payload.Body,
               sessionId: sessionId,
               ubiquity: {
+                sessionId,
                 twilio: payload
               }
             }
