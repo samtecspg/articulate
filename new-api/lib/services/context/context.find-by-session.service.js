06d6212bc630d43f07b1ad8bf5494f299034faec
Malavé <luis.malave@gmail.com>
2018-11-16 14:52:36 +0100

    - Search by session id not correctly returns a 404 when is not found

--- new-api/lib/services/context/context.find-by-session.service.js
+++ new-api/lib/services/context/context.find-by-session.service.js
@@ -2,6 +2,7 @@ import {
     MODEL_CONTEXT,
     MODEL_FRAME
 } from '../../../util/constants';
+import NotFoundErrorHandler from '../../errors/global.not-found-error';
 import RedisErrorHandler from '../../errors/redis.error-handler';
 
 module.exports = async function ({ sessionId, loadFrames = false, returnModel = false }) {
@@ -11,14 +12,18 @@ module.exports = async function ({ sessionId, loadFrames = false, returnModel = 
     const { globalService } = await this.server.services();
     try {
         await Model.findBySessionId({ sessionId });
-        //Only load frames if we are NOT returning the model, or else we can't create and object with a frames list.
-        if (!returnModel && loadFrames) {
-            const frames = await globalService.loadAllLinked({ parentModel: Model, model: MODEL_FRAME, returnModel });
-            const context = Model.allProperties();
-            context.frames = frames;
-            return context;
+        if (Model.inDb) {
+            //Only load frames if we are NOT returning the model, or else we can't create and object with a frames list.
+            if (!returnModel && loadFrames) {
+                const frames = await globalService.loadAllLinked({ parentModel: Model, model: MODEL_FRAME, returnModel });
+                const context = Model.allProperties();
+                context.frames = frames;
+                return context;
+            }
+            return returnModel ? Model : Model.allProperties();
         }
-        return returnModel ? Model : Model.allProperties();
+        return Promise.reject(NotFoundErrorHandler({ model: MODEL_CONTEXT, id: sessionId }));
+
     }
     catch (error) {
         throw RedisErrorHandler({ error });
