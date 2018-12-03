55754d2f1b0bbf8b8a278155ed1fa5c9bfbe606f
Daniel Calvo <dcalvom@gmail.com>
2018-12-03 14:52:52 -0600

    Cleaned missing API page

--- new-ui/app/containers/MissingAPIPage/index.js
+++ new-ui/app/containers/MissingAPIPage/index.js
@@ -1,22 +1,68 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { FormattedMessage } from 'react-intl';
+import { Grid, Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
 
 import imgMissingAPI from '../../images/missing-api.svg';
 import messages from './messages';
 
-export default class MissingAPIPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+const styles = {
+  mainContainer: {
+    marginTop: '60px'
+  },
+  image: {
+    width: '300px',
+    height: '300px',
+    display: 'block',
+    margin: 'auto',
+    width: '50%'
+  },
+  needHelpLink: {
+    cursor: 'pointer',
+    left: -15,
+    fontSize: '14px',
+    fontFamily: 'Montserrat',
+    fontWeight: 400,
+    position: 'relative',
+  }
+}
+
+class MissingAPIPage extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
   render() {
+    const { classes } = this.props;
     return (
-      <Grid>
-        <p>{messages.missingAPITitle.defaultMessage}</p>
-        <p>{messages.missingAPIDescription.defaultMessage}</p>
-        <img className="error-img" src={imgMissingAPI} alt="" />
-        <p className="error-paragraph">{messages.missingAPIParagraph.defaultMessage}</p>
-        <div className="error-btn-container">
-          <a target="_blank" className="btn-floating btn-large" href="https://samtecspg.github.io/articulate/">See Docs</a>
-        </div>
+      <Grid className={classes.mainContainer} container justify='center' spacing={8}>
+        <Grid item xs={12}>
+          <Typography align='center'>
+            <FormattedMessage {...messages.missingAPITitle} />
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <img className={classes.image} src={imgMissingAPI} alt="" />
+        </Grid>
+        <Grid item xs={6}>
+          <Typography align='center'>
+            <FormattedMessage {...messages.missingAPIParagraph} />
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+        </Grid>
+        <Grid item xs={12}>
+            <a className={classes.needHelpLink} target="_blank" href='https://samtecspg.github.io/articulate/'>
+              <Typography align='center'>
+                <FormattedMessage {...messages.needHelp} />
+              </Typography>
+            </a>
+        </Grid>
       </Grid>
     );
   }
 }
+
+MissingAPIPage.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(MissingAPIPage);
