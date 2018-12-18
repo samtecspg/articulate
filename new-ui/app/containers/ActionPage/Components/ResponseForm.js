aae3e69f0a551ff67ff4a801a66537b08602987c
Daniel Calvo <dcalvom@gmail.com>
2018-12-17 23:14:33 -0600

    Fixed multiple bugs on action chaining

--- new-ui/app/containers/ActionPage/Components/ResponseForm.js
+++ new-ui/app/containers/ActionPage/Components/ResponseForm.js
@@ -7,13 +7,13 @@ import { withStyles } from "@material-ui/core/styles";
 
 import SingleHighlightedSaying from './SingleHighlightedSaying';
 import ResponseSettings from 'components/ResponseSettings';
+import ResponseRow from './ResponseRow';
 
 import messages from "../messages";
 
 import playHelpIcon from "../../../images/play-help-icon.svg";
 import singleQuotesIcon from "../../../images/single-quotes-icon.svg";
 import trashIcon from '../../../images/trash-icon.svg';
-import addActionIcon from '../../../images/add-action-icon.svg';
 
 const styles = {
   headerContainer: {
@@ -94,44 +94,7 @@ const styles = {
   },
   postFormatContainer: {
     marginTop: '20px',
-  },
-  actionBackgroundContainer: {
-    '&:hover': {
-      backgroundColor: '#4e4e4e',
-      color: '#fff',
-    },
-    margin: '0px 5px 0px 5px',
-    fontSize: '12px',
-    padding: '4px 8px 4px 10px',
-    backgroundColor: '#e2e5e7',
-    display: 'inline-block',
-    position: 'relative',
-    borderRadius: '5px',
-    marginTop: '2px',
-  },
-  actionLabel: {
-    textDecoration: 'none',
-    color: 'inherit',
-  },
-  deleteActionX: {
-    '&:hover': {
-      fontWeight: 'bold',
-    },
-    paddingLeft: '5px',
-    fontWeight: 300,
-    cursor: 'pointer',
-  },
-  addActionIcon: {
-    '&:hover': {
-      filter: 'invert(1)',
-    },
-    cursor: 'pointer',
-    verticalAlign: 'middle',
-  },
-  response: {
-    paddingRight: '5px',
-    lineHeight: '1.5',
-  },
+  }
 };
 
 /* eslint-disable react/prefer-stateless-function */
@@ -236,63 +199,14 @@ class ResponseForm extends React.Component {
                         {action.responses.map((response, responseIndex) => (
                           <TableRow key={`${response}_${responseIndex}`}>
                             <TableCell>
-                              <Grid container>
-                                <Grid item xs={12}>
-                                  <span className={classes.response}>
-                                    {response.textResponse}
-                                  </span>
-                                  {response.actions.map((action, actionIndex) => {
-                                    return (
-                                      <div key={`responseAction_${actionIndex}`} className={classes.actionBackgroundContainer}>
-                                        <span
-                                          className={classes.actionLabel}
-                                        >{action}</span>
-                                        <a onClick={() => { this.props.onUnchainActionFromResponse(responseIndex, actionIndex) }} className={classes.deleteActionX}>x</a>
-                                      </div>
-                                    )
-                                  })}
-                                  <img
-                                    onClick={(evt) => this.setState({
-                                      anchorEl: evt.target,
-                                      openActions: true,
-                                    })}
-                                    className={classes.addActionIcon} src={addActionIcon}
-                                  />
-                                  <FormControl>
-                                    <Select
-                                      style={{
-                                        display:'none',
-                                      }}
-                                      open={this.state.openActions}
-                                      onClose={() => this.setState({
-                                        openActions: false,
-                                        anchorEl: null,
-                                      })}
-                                      onOpen={(evt) => this.setState({
-                                        anchorEl: evt.target,
-                                        openActions: true,
-                                      })}
-                                      value={10}
-                                      onChange={(evt) => { evt.preventDefault(); this.props.onChainActionToResponse(responseIndex, evt.target.value)}}
-                                      MenuProps={{
-                                        style:{
-                                          minHeight: '300px',
-                                          maxHeight: '300px',
-                                        },
-                                        anchorEl: this.state.anchorEl,
-                                      }}
-                                    >
-                                      {
-                                        this.props.agentActions.map((agentAction) => (
-                                          response.actions.indexOf(agentAction.actionName) === -1 && agentAction.actionName !== action.actionName ?
-                                            <MenuItem style={{width: '200px'}} key={`action_${agentAction.id}`} value={agentAction.actionName}>{agentAction.actionName}</MenuItem> :
-                                            null
-                                        ))
-                                      }
-                                    </Select>
-                                  </FormControl>
-                                </Grid>
-                              </Grid>
+                              <ResponseRow 
+                                response={response}
+                                responseIndex={responseIndex}
+                                action={action}
+                                agentActions={this.props.agentActions}
+                                onChainActionToResponse={this.props.onChainActionToResponse}
+                                onUnchainActionFromResponse={this.props.onUnchainActionFromResponse}
+                              />
                             </TableCell>
                             <TableCell className={classes.deleteCell}>
                               <img onClick={() => { this.props.onDeleteResponse(responseIndex) }} className={classes.deleteIcon} src={trashIcon} />
