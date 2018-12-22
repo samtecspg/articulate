809ba39c98a66bdbae7c81763ca06d79c1782c18
Daniel Calvo <dcalvom@gmail.com>
2018-12-22 01:41:05 -0600

    Change webhook wording

--- new-ui/app/containers/AgentPage/messages.js
+++ new-ui/app/containers/AgentPage/messages.js
@@ -133,7 +133,7 @@ export default defineMessages({
   },
   webhookSettingDescription: {
     id: 'app.containers.AgentPage.component.AgentSettingsForm.webhookSettingDescription',
-    defaultMessage: 'If you want to define a global webhook that will be called each time the user talks with your agent then you can do it here. If you define a webhook for an action, then that webhook will override this configuration.',
+    defaultMessage: 'If you would like to define a global webhook that will be called each time the user talks with your agent, then you may do it here. If you define a webhook for an action, then that webhook will override this configuration.',
   },
   multipleIntentRecognition: {
     id: 'app.containers.AgentPage.component.AgentSettingsForm.multipleIntentRecognition',
