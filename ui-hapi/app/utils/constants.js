8e8544dcdf9deac4294f9b8b8fbba7d1e82c637f
Malavé <luis.malave@gmail.com>
2019-03-06 14:06:54 +0100

    Removed temporary migration project

--- ui-hapi/app/utils/constants.js
+++
@@ -1,4 +0,0 @@
-export const RESTART_ON_REMOUNT = '@@saga-injector/restart-on-remount';
-export const DAEMON = '@@saga-injector/daemon';
-export const ONCE_TILL_UNMOUNT = '@@saga-injector/once-till-unmount';
-export const ACTION_INTENT_SPLIT_SYMBOL = '+__+';
