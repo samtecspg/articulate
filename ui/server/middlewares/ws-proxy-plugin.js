fccad3f6206777d1ec113fc19157f7619258f66c
Malavé <luis.malave@gmail.com>
2019-04-02 16:45:04 +0200

    Removed dependency of the between the UI-Server->API websocket and the UI-Client->UI-Server websocket during startup. Now this will be check on runtime and won't cause one or bot not to be initialized properly

--- ui/server/middlewares/ws-proxy-plugin.js
+++ ui/server/middlewares/ws-proxy-plugin.js
@@ -8,36 +8,17 @@ const name = 'ws-proxy-plugin';
 exports.plugin = {
   name,
   async register(server) {
-    try {
-      const subscriptions = [];
-      // Setup connection with API WS
-      const apiURL = new URL('', API_URL);
-      const apiClient = new Nes.Client(`ws://${apiURL.host}`);
-      apiClient.onError = (err) => {
-        logger.error(`[WS] Error ${API_URL}`);
-        logger.error(err);
-      };
-      apiClient.onConnect = () => {
-        logger.log(`[WS] Connected to ${API_URL}`);
-      };
-      apiClient.onDisconnect = (willReconnect, log) => {
-        logger.log(`[WS] Disconnect from ${API_URL}`);
-        logger.log(log);
-        logger.log(`[WS] Will Reconnect = ${willReconnect}`);
-      };
-      apiClient.onHeartbeatTimeout = (willReconnect) => {
-        logger.log(`[WS] Heartbeat Timeout from ${API_URL}`);
-        logger.log(`[WS] Will Reconnect = ${willReconnect}`);
-      };
-      await apiClient.connect({
-        delay: 1000,
-      });
+    let apiClient;
+    const subscriptions = [];
 
-      // Setup local WS
+    const initServer = async ({ server }) => {
       await server.register({ plugin: Nes, options: {} });
 
       const onSubscribe = (socket, path) => {
-
+        if (_.isNil(apiClient)) {
+          logger.log('[WS] [onSubscribe] API WS Not connected ');
+          throw new Error('API Client not ready');
+        }
         const clientPath = path.substring(PROXY_ROUTE_PREFIX.length);
         subscriptions.push({ id: socket.id, path });
         apiClient.subscribe(clientPath, (update) => {
@@ -46,8 +27,11 @@ exports.plugin = {
       };
 
       const onUnsubscribe = (socket, path) => {
-
         _.remove(subscriptions, item => item.id === socket.id && item.path === path);
+        if (_.isNil(apiClient)) {
+          logger.log('[WS] [onUnsubscribe] API WS Not connected ');
+          return;
+        }
         const samePath = subscriptions.find((item) => item.path === path);
         if (!samePath) {
           apiClient.unsubscribe(path.substring(PROXY_ROUTE_PREFIX.length));
@@ -56,11 +40,45 @@ exports.plugin = {
 
       server.subscription(`${PROXY_ROUTE_PREFIX}/agent/{id}`, { onSubscribe, onUnsubscribe });
       server.subscription(`${PROXY_ROUTE_PREFIX}/agent/{id}/doc`, { onSubscribe, onUnsubscribe });
+    };
+    
+    const initClient = async () => {
+      // Setup connection with API WS
+      const apiURL = new URL('', API_URL);
+      const client = new Nes.Client(`ws://${apiURL.host}`);
+      client.onError = (err) => {
+        logger.error(`[WS] Error ${API_URL}`);
+        logger.error(err);
+        apiClient = null;
+      };
+      client.onConnect = async () => {
+        logger.log(`[WS] Connected to ${API_URL}`);
+        apiClient = client;
+      };
+      client.onDisconnect = (willReconnect, log) => {
+        logger.log(`[WS] Disconnect from ${API_URL}`);
+        logger.log(log);
+        logger.log(`[WS] Will Reconnect = ${willReconnect}`);
+        subscriptions.splice(0, subscriptions.length);
+        apiClient = null;
+      };
+      client.onHeartbeatTimeout = (willReconnect) => {
+        logger.log(`[WS] Heartbeat Timeout from ${API_URL}`);
+        logger.log(`[WS] Will Reconnect = ${willReconnect}`);
+        apiClient = null;
+      };
+      await client.connect({
+        delay: 1000
+      });
+    };
+    try {
+      await initServer({ server });
+      await initClient();
 
     }
     catch (e) {
       logger.error(name);
       logger.error(e);
     }
-  },
+  }
 };
