cd43b3bfa36543f3399fea8199d3964e12594779
Malavé <luis.malave@gmail.com>
2019-06-30 16:34:05 +0300

    - After login and signup the page will be reloaded, this will cause the WS to reconnect and any initial queries to be successful.

--- api/lib/routes/user/user.create.route.js
+++ api/lib/routes/user/user.create.route.js
@@ -5,15 +5,21 @@ import UserValidator from '../../validators/user.validator';
 module.exports = {
     method: 'post',
     path: `/${ROUTE_USER_ACCOUNT}`,
+
     options: {
         tags: ['api'],
         validate: UserValidator.create,
+        auth: {
+            mode: 'optional'
+        },
         handler: async (request) => {
 
             const { userService } = await request.services();
             try {
                 const { identity, ...rest } = request.payload;
-                return await userService.create({ data: rest, identity, filterSensitiveData: true });
+                const user = await userService.create({ data: rest, identity, filterSensitiveData: true });
+                await request.cookieAuth.set({ id: user.id, name: user.name, email: user.email });
+                return user;
             }
             catch ({ message, statusCode }) {
                 return new Boom(message, { statusCode });
