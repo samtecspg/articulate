042136352187f142c6e9783902088195d2eb6f25
Daniel Calvo <dcalvom@gmail.com>
2019-08-15 11:12:30 -0600

    Added keyword flow to refactored converse endpoint

--- api/lib/services/agent/agent.converse.service.js
+++ api/lib/services/agent/agent.converse.service.js
@@ -66,7 +66,7 @@ const getModifierData = ({ recognizedModifierName, CSO }) => {
 }
 
 
-const getActionToModify = ({ recognizedModifier, CSO }) => {
+const getActionToModify = ({ recognizedModifier, recognizedKeyword, CSO }) => {
 
     let actionToModify;
 
@@ -79,7 +79,7 @@ const getActionToModify = ({ recognizedModifier, CSO }) => {
         const actionData = getActionData({ actionName: action.name, CSO });
         const isCandidate = actionData.slots.some((actionSlot) => {
 
-            return actionSlot.keyword === recognizedModifier.keyword;
+            return actionSlot.keyword === recognizedModifier ? recognizedModifier.keyword : recognizedKeyword.keyword;
         });
         if (isCandidate){
             return {
@@ -148,8 +148,11 @@ module.exports = async function ({ id, sessionId, text, timezone, debug = false,
                 }
             });
         }
+
+        //We extract the keywords from the best rasa result
+        CSO.recognizedKeywords = await agentService.converseGetKeywordsFromRasaResults({ rasaResults: [ CSO.rasaResult ] })
     
-        if (CSO.rasaResult.action && CSO.rasaResult.action.name){
+        if (CSO.rasaResult.action && CSO.rasaResult.action.name || CSO.recognizedKeywords.length > 0){
             
             updateLifespanOfSlots({ CSO });
             /*
@@ -180,9 +183,6 @@ module.exports = async function ({ id, sessionId, text, timezone, debug = false,
                 });
             });
     
-            //We extract the keywords from the best rasa result
-            CSO.recognizedKeywords = await agentService.converseGetKeywordsFromRasaResults({ rasaResults: [ CSO.rasaResult ] })
-    
             /*
             * Given that we don't have actions + modifiers models, we know that if there is a modifier, then we know that this is
             * what we need to process. Remember we don't support multi modifier models, but, we iterate as if we have it
@@ -207,6 +207,35 @@ module.exports = async function ({ id, sessionId, text, timezone, debug = false,
                     }
                     else {
                         //Return fallback because this means user started the conversation with a modifier
+                        CSO.response = await agentService.converseGenerateResponseFallback({ CSO });
+                        await agentService.converseSendResponseToUbiquity({ CSO });
+                    }
+                }
+            }
+
+            /*
+            * This is known as the keyword flow 
+            * In case no modifiers neither actions were recognized we are going to use recognized keywords to try to fulfill
+            * an action in the queue that can be fulfilled with those recognized values
+            */
+            if (CSO.recognizedKeywords.length > 0 && CSO.recognizedModifiers.length === 0 && CSO.recognizedActions.length === 0){
+                for (const recognizedKeyword of CSO.recognizedKeywords){
+                    if (CSO.context.actionQueue.length > 0){
+
+                        CSO.actionToModify = getActionToModify({ recognizedKeyword, CSO });
+    
+                        if (CSO.actionToModify && CSO.actionToModify.actionData.slots && CSO.actionToModify.actionData.slots.length > 0){
+                            CSO.currentAction = CSO.context.actionQueue[CSO.actionToModify.index];
+                            await agentService.converseFillActionSlots({ actionData: CSO.actionToModify.actionData, CSO });
+                        }
+                        else {
+                            //Return fallback because this means a keyword was recognized for an action that doesn't exists in the queue
+                            CSO.response = await agentService.converseGenerateResponseFallback({ CSO });
+                            await agentService.converseSendResponseToUbiquity({ CSO });
+                        }
+                    }
+                    else {
+                        //Return fallback because this means user started the conversation with the keyword flow
                         CSO.response = await agentService.converseGenerateResponseFallback({ CSO });
                         await agentService.converseSendResponseToUbiquity({ CSO });
                     }
