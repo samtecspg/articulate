ce76baaf3ee399726aefe4fc55f4e2686b9ac10f
Daniel Calvo <dcalvom@gmail.com>
2019-08-30 00:10:48 -0600

    allow empty string in postscript

--- api/lib/models/webhook.model.js
+++ api/lib/models/webhook.model.js
@@ -22,7 +22,7 @@ class WebhookModel {
             }),
             webhookUser: Joi.string().allow(''),
             webhookPassword: Joi.string().allow(''),
-            postScript: Joi.string().trim(),
+            postScript: Joi.string().trim().allow(''),
             creationDate: Joi
                 .string(),
             modificationDate: Joi
