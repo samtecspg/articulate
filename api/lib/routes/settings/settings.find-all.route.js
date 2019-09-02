11d2a30f97e5bb2194830fbc1fa83e10086b585c
dakavisa <dakavisa623@gmail.com>
2019-09-02 15:53:29 -0600

    disable auth for settings and update-by-name

--- api/lib/routes/settings/settings.find-all.route.js
+++ api/lib/routes/settings/settings.find-all.route.js
@@ -5,21 +5,21 @@ import { ROUTE_SETTINGS } from '../../../util/constants';
 //const logger = require('../../../util/logger')({ name: `route:agent:create` });
 
 module.exports = {
-    method: 'get',
-    path: `/${ROUTE_SETTINGS}`,
-    options: {
-        tags: ['api'],
-        handler: async (request) => {
-
-            const { settingsService } = await request.services();
-            try {
-                return await settingsService.findAll();
-            }
-            catch ({ message, statusCode }) {
-
-                return new Boom(message, { statusCode });
-            }
-        }
+  method: 'get',
+  path: `/${ROUTE_SETTINGS}`,
+  options: {
+    tags: ['api'],
+    auth: {
+      strategy: 'session',
+      mode: 'optional'
+    },
+    handler: async request => {
+      const { settingsService } = await request.services();
+      try {
+        return await settingsService.findAll();
+      } catch ({ message, statusCode }) {
+        return new Boom(message, { statusCode });
+      }
     }
+  }
 };
-
--- api/lib/routes/settings/settings.update-by-name.route.js
+++ api/lib/routes/settings/settings.update-by-name.route.js
@@ -1,28 +1,30 @@
 import Boom from 'boom';
-import {
-    PARAM_NAME,
-    ROUTE_SETTINGS
-} from '../../../util/constants';
+import { PARAM_NAME, ROUTE_SETTINGS } from '../../../util/constants';
 import SettingsValidator from '../../validators/settings.validator';
 
 //const logger = require('../../../util/logger')({ name: `route:agent:create` });
 
 module.exports = {
-    method: 'put',
-    path: `/${ROUTE_SETTINGS}/{${PARAM_NAME}}`,
-    options: {
-        tags: ['api'],
-        validate: SettingsValidator.updateByName,
-        handler: async (request) => {
-
-            const { settingsService } = await request.services();
-            const { [PARAM_NAME]: name } = request.params;
-            try {
-                return await settingsService.updateByName({ name, value: request.payload });
-            }
-            catch ({ message, statusCode }) {
-                return new Boom(message, { statusCode });
-            }
-        }
+  method: 'put',
+  path: `/${ROUTE_SETTINGS}/{${PARAM_NAME}}`,
+  options: {
+    tags: ['api'],
+    auth: {
+      strategy: 'session',
+      mode: 'optional'
+    },
+    validate: SettingsValidator.updateByName,
+    handler: async request => {
+      const { settingsService } = await request.services();
+      const { [PARAM_NAME]: name } = request.params;
+      try {
+        return await settingsService.updateByName({
+          name,
+          value: request.payload
+        });
+      } catch ({ message, statusCode }) {
+        return new Boom(message, { statusCode });
+      }
     }
+  }
 };
