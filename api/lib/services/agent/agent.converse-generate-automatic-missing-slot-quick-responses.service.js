a6391e8f75316cda139df2e769fae4feeeb2b003
dakavisa <dakavisa623@gmail.com>
2019-12-26 18:49:14 -0600

    Fixed slots suggestion issue with random

--- api/lib/services/agent/agent.converse-generate-automatic-missing-slot-quick-responses.service.js
+++ api/lib/services/agent/agent.converse-generate-automatic-missing-slot-quick-responses.service.js
@@ -1,12 +1,27 @@
 module.exports = function({ CSO, missingSlot }) {
   const missingSlotKeywordName = missingSlot.keyword;
-  const keywordToFillSlot = CSO.agent.keywords.find(keyword => {
-    return keyword.keywordName === missingSlotKeywordName;
-  });
-  const quickResponsesGenerated = keywordToFillSlot.examples
+  const keywordToFillSlot = shuffleArray(
+    CSO.agent.keywords.find(keyword => {
+      return keyword.keywordName === missingSlotKeywordName;
+    })
+  );
+  var quickResponsesGenerated = shuffleArray(keywordToFillSlot.examples);
+  quickResponsesGenerated = quickResponsesGenerated
     .slice(0, CSO.agent.settings.generateSlotsQuickResponsesMax)
     .map(example => {
       return example.value;
     });
   return quickResponsesGenerated;
+};
+
+const shuffleArray = array => {
+  if (array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  } else {
+    return [];
+  }
 };
