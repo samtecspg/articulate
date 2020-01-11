d7ad46972311a39f2a245cee03a69b0fc5af3d22
dakavisa <dakavisa623@gmail.com>
2020-01-11 13:27:16 -0600

    Added reply boilerplate code for slack

--- api/lib/channels/slack/reply.js
+++ api/lib/channels/slack/reply.js
@@ -1,11 +1,11 @@
 const { WebClient } = require('@slack/client');
 
-module.exports = async function({ connection, event, response }) {
+module.exports = async function ({ connection, event, response }) {
 
     const web = new WebClient(connection.details.botAccessToken);
 
     let attachments;
-    if (response.quickResponses){
+    if (response.quickResponses) {
         const actions = response.quickResponses.map((quickResponse, index) => {
 
             return {
@@ -27,5 +27,53 @@ module.exports = async function({ connection, event, response }) {
         ]
     }
 
-    web.chat.postMessage({ channel: event.event.channel, text: response.textResponse, attachments });
+    var blocks = [];
+    //blocks.push(createImageBlock());
+    blocks.push(createTextBlock(response.textResponse));
+    blocks.push(createDividerBlock());
+    blocks.push(createCardBlock());
+    web.chat.postMessage({ channel: event.event.channel, text: response.textResponse, attachments, blocks });
+}
+
+const createTextBlock = (response) => {
+    return {
+        type: "section",
+        text: {
+            type: "plain_text",
+            text: response,
+            emoji: true
+        }
+    }
+}
+
+const createImageBlock = () => {
+    var blocks = [];
+    var imageInfo = {
+        type: "image",
+        image_url: "https://cdn.repretel.com/files/2019/01/16/herediano_0.jpg",
+        alt_text: "Mambo nunez"
+    }
+    blocks.push(imageInfo)
+    return blocks;
+}
+
+const createDividerBlock = () => {
+    return {
+        type: "divider"
+    }
+}
+
+const createCardBlock = () => {
+    return {
+        type: "section",
+        text: {
+            type: "mrkdwn",
+            text: "*<fakeLink.toHotelPage.com|Windsor Court Hotel>*\n★★★★★\n$340 per night\nRated: 9.4 - Excellent"
+        },
+        accessory: {
+            type: "image",
+            image_url: "https://cdn.repretel.com/files/2019/01/16/herediano_0.jpg",
+            alt_text: "Windsor Court Hotel thumbnail"
+        }
+    }
 }
