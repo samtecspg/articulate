e8a7f8f9cb9b650a9d42a7e0413087e82b9d41f6
dakavisa <dakavisa623@gmail.com>
2020-01-11 20:58:30 -0600

    Added rich responses for slack, just quick responses pending

--- api/lib/channels/slack/reply.js
+++ api/lib/channels/slack/reply.js
@@ -4,57 +4,103 @@ module.exports = async function ({ connection, event, response }) {
 
     const web = new WebClient(connection.details.botAccessToken);
 
-    let attachments;
-    if (response.quickResponses) {
-        const actions = response.quickResponses.map((quickResponse, index) => {
-
-            return {
-                name: `{ "api_app_id": "${event.api_app_id}", "quickResponse": ${index + 1} }`,
-                text: quickResponse,
-                type: 'button',
-                value: quickResponse
-            }
-        });
-        attachments = [
-            {
-                text: "",
-                fallback: "Quick response not selected",
-                callback_id: "quickResponse",
-                color: "#3AA3E3",
-                attachment_type: "default",
-                actions
-            }
-        ]
-    }
+    /* let attachments;
+     if (response.quickResponses) {
+         const actions = response.quickResponses.map((quickResponse, index) => {
+ 
+             return {
+                 name: `{ "api_app_id": "${event.api_app_id}", "quickResponse": ${index + 1} }`,
+                 text: quickResponse,
+                 type: 'button',
+                 value: quickResponse
+             }
+         });
+         attachments = [
+             {
+                 text: "",
+                 fallback: "Quick response not selected",
+                 callback_id: "quickResponse",
+                 color: "#3AA3E3",
+                 attachment_type: "default",
+                 actions
+             }
+         ]
+     }
+     */
 
     var blocks = [];
+    if (response.richResponses && response.richResponses.length > 0) {
+        blocks.push(createTextBlock(response.textResponse));
+        blocks.push(createDividerBlock());
+        for (const richResponse of response.richResponses) {
+            if (richResponse.type === 'image') {
+                blocks.push(createImageBlock(richResponse));
+            }
+            if (richResponse.type === 'video') {
+                blocks.push(createVideoBlock(richResponse));
+            }
+            if (richResponse.type === 'buttons') {
+                blocks.push(createButtonBlock(richResponse));
+            }
+            if (richResponse.type === 'audio') {
+                blocks.push(createAudioBlock(richResponse));
+            }
+            if (richResponse.type === 'cardsCarousel') {
+                for (const card of richResponse.data) {
+                    blocks.push(createDividerBlock());
+                    blocks.push(createCardBlock(card));
+                }
+            }
+            if (richResponse.type === 'collapsible') {
+                for (const collapsible of richResponse.data) {
+                    blocks.push(createDividerBlock());
+                    blocks.push(createCollapsibleBlock(collapsible));
+                }
+            }
+            blocks.push(createDividerBlock());
+        }
+    }
     //blocks.push(createImageBlock());
-    blocks.push(createTextBlock(response.textResponse));
-    blocks.push(createDividerBlock());
-    blocks.push(createCardBlock());
-    web.chat.postMessage({ channel: event.event.channel, text: response.textResponse, attachments, blocks });
+    web.chat.postMessage({ channel: event.event.channel, text: response.textResponse, blocks });
 }
 
-const createTextBlock = (response) => {
+const createTextBlock = (text) => {
     return {
         type: "section",
         text: {
             type: "plain_text",
-            text: response,
+            text: text,
             emoji: true
         }
     }
 }
 
-const createImageBlock = () => {
-    var blocks = [];
-    var imageInfo = {
+const createImageBlock = (richResponse) => {
+    return {
         type: "image",
-        image_url: "https://cdn.repretel.com/files/2019/01/16/herediano_0.jpg",
-        alt_text: "Mambo nunez"
+        image_url: richResponse.data.imageURL,
+        alt_text: " "
     }
-    blocks.push(imageInfo)
-    return blocks;
+}
+
+const createVideoBlock = (richResponse) => {
+    return {
+        type: "section",
+        text: {
+            "type": "mrkdwn",
+            "text": "<" + richResponse.data.video + ">"
+        }
+    }
+}
+
+const createAudioBlock = (richResponse) => {
+    return {
+        type: "section",
+        text: {
+            "type": "mrkdwn",
+            "text": "<" + richResponse.data.audio + ">"
+        }
+    }
 }
 
 const createDividerBlock = () => {
@@ -63,17 +109,54 @@ const createDividerBlock = () => {
     }
 }
 
-const createCardBlock = () => {
+const createCardBlock = (card) => {
     return {
         type: "section",
         text: {
             type: "mrkdwn",
-            text: "*<fakeLink.toHotelPage.com|Windsor Court Hotel>*\n★★★★★\n$340 per night\nRated: 9.4 - Excellent"
+            text: createCardText(card)
         },
         accessory: {
             type: "image",
-            image_url: "https://cdn.repretel.com/files/2019/01/16/herediano_0.jpg",
-            alt_text: "Windsor Court Hotel thumbnail"
+            image_url: card.imageURL,
+            alt_text: card.title
         }
+    }
+}
+
+const createCardText = (card) => {
+    return "*<" + card.linkURL + "|" + card.title + ">*\n" + card.description
+}
+
+const createCollapsibleBlock = (collapsible) => {
+    return {
+        type: "section",
+        text: {
+            type: "mrkdwn",
+            text: createCollapsibleText(collapsible)
+        }
+    }
+}
+
+const createCollapsibleText = (collapsible) => {
+    return "*" + collapsible.title + "*\n" + collapsible.content
+}
+
+const createButtonBlock = (richResponse) => {
+    var elements = [];
+    for (const button of richResponse.data) {
+        elements.push({
+            "type": "button",
+            "text": {
+                "type": "plain_text",
+                "text": button.label
+            },
+            "url": button.linkURL
+        })
+    }
+
+    return {
+        type: "actions",
+        elements
     }
 }
